b4d14b37e911386767defb9df9f6c889c35499bf
Muhammad Raihan Satrio Putra Pamungkas <62191088+satrio-pamungkas@users.noreply.github.com>
2021-02-03 20:43:01 +0700

    Nyoba pake java
    -> Efek PBO

---
+++ Kode/challenge-3/challenge-3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+// Ganti nama file ke 'App.java'
+public class App {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        String nama[] = new String [n];
+
+        for(int i=0; i<n; i++) {
+            nama[i] = input.next();
+        }
+
+        for(int i=0; i<n; i++) {
+            int posisi = 1;
+            for(int j=0; j<i; j++) {
+                if((nama[i].compareTo(nama[j]) > 0)) {
+                    posisi = posisi + 1;
+                }
+            }
+
+            System.out.println(posisi);
+        }
+    }
+}
